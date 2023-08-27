46c0d932f39b6c66f8827aa4b6e5b84b509c6dea
Glenn Wiley <glenn.wiley@wwt.com>
2023-08-27 14:58:11 -0500

    Set up Auction DTO objects and configure AutoMapper to create the mappings

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => 
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
